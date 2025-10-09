2a2f980bf91dfaab0451404d1c5307aee01e0aa1
Ajay-singh-github <ajaysikata@gmail.com>
2025-10-09 20:22:17 +0530

    image

--- src/screens/Category.jsx
+++ src/screens/Category.jsx
@@ -1,16 +1,74 @@
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { styled } from '@mui/material/styles';
+import { useState } from 'react';
+import nitesh2 from "../assets/nitesh2.jpg"
+import logo from "../assets/logo.png"
+import image from "../assets/image.png"
+import DeleteIcon from '@mui/icons-material/Delete';
 
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+});
 export default function Category() {
+    const [imagename, setImageName] = useState('')
+
+    const handlePicture = (e) => {
+        if(e.target.files[0].name==="nitesh2.jpg"){
+            setImageName(nitesh2)
+        }
+        else if(e.target.files[0].name==="logo.png"){
+            setImageName(logo)
+        }
+        else if(e.target.files[0].name==="image.png"){
+            setImageName(image)
+        }
+        else{
+            setImageName("")
+        }
+        console.log(e.target.files[0].name)
+    }
+
+    const handleImageRemove=()=>{
+        setImageName("")
+    }
+
+    console.log(`../assets/${imagename}`)
     return (
         <div className="h-[100%] w-[100%] flex justify-center items-center">
-            <div className="bg-neutral-100 h-[60%] w-[50%] rounded-2xl gap-5 flex flex-col justify-center items-center" >
+            <div className="bg-neutral-100 h-[70%] w-[50%] rounded-2xl gap-5 flex flex-col justify-center items-center" >
                 <TextField id="outlined-basic" label="Enter Category" variant="outlined" className='w-[70%]' />
                 <TextField id="outlined-basic" label="Enter Description" variant="outlined" className='w-[70%]' />
-                <Button variant="contained">Submit</Button>
-                {/* <input type="text" placeholder="Enter Category Name" className="w-[50%]"/> */}
-                {/* <input type="text" placeholder="Enter Description" className="w-[50%]" /> */}
-                {/* <input type="submit" value="Submit" className="bg-blue-400 w-[100px] rounded"/> */}
+                <Button
+                    className='w-[70%]'
+                    component="label"
+                    role={undefined}
+                    variant="contained"
+                    tabIndex={-1}
+                    startIcon={<CloudUploadIcon />}
+                >
+                    Upload files
+                    <VisuallyHiddenInput
+                        type="file"
+                        onChange={handlePicture}
+                    />
+                </Button>
+                {imagename.length<=1?"":
+                <div className='flex gap-2'>
+                <img  width={70} className='rounded cursor-pointer' height={70} src={imagename} /> <DeleteIcon onClick={()=>handleImageRemove()} className='cursor-pointer'/>
+                </div>
+                }
+
+                <Button variant="outlined" >Add Category</Button>
             </div>
         </div>)
 }
--- src/screens/SubCategory.jsx
+++ src/screens/SubCategory.jsx
@@ -1,9 +1,67 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { styled } from '@mui/material/styles';
 
-export default function Subcategory()
-{
-   
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
 
-    return(<div >
-        hello i am subcategory page.
+export default function Subcategory() {
+
+
+    return (<div className="h-[100%] w-[100%] flex justify-center items-center">
+        <div className="bg-neutral-100 h-[60%] w-[50%] rounded-2xl gap-5 flex flex-col justify-center items-center" >
+            <Box sx={{ minWidth: 120 }} className='w-[70%]'>
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Category</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        // value={age}
+                        label="Category"
+                        // onChange={handleChange}
+                    >
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                </FormControl>
+            </Box>
+
+
+            <TextField id="outlined-basic" label="Enter Subcategory" variant="outlined" className='w-[70%]' />
+            <TextField id="outlined-basic" label="Enter Description" variant="outlined" className='w-[70%]' />
+            <Button
+                    className='w-[70%]'
+                    component="label"
+                    role={undefined}
+                    variant="contained"
+                    tabIndex={-1}
+                    startIcon={<CloudUploadIcon />}
+                >
+                    Upload files
+                    <VisuallyHiddenInput
+                        type="file"
+                    />
+                </Button>
+            <Button variant="outlined">Add Subcategory</Button>
+            {/* <input type="text" placeholder="Enter Category Name" className="w-[50%]"/> */}
+            {/* <input type="text" placeholder="Enter Description" className="w-[50%]" /> */}
+            {/* <input type="submit" value="Submit" className="bg-blue-400 w-[100px] rounded"/> */}
+        </div>
     </div>)
 }
